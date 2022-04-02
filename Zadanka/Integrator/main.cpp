ae0fbc7aff7f6e5a3736ea76a8c997d7d4b6be28
Kamil Kowalski <kamilkowalski314@gmail.com>
2022-04-02 17:39:21 +0200

    monte carlo

---
+++ Zadanka/Integrator/main.cpp
@@ -0,0 +1,102 @@
+#include <future>
+#include <iostream>
+#include <math.h>
+#include <regex>
+#include <thread>
+#include <vector>
+
+struct PolyTerm {
+  double coeff;
+  double power;
+};
+
+double computePolynomial(const std::vector<PolyTerm> &terms, double x) {
+  double result = 0;
+  for (const auto &term : terms) {
+    result += term.coeff * pow(x, term.power);
+  }
+  return result;
+}
+
+PolyTerm toTerm(const std::string &coefficient, const std::string &power) {
+  return PolyTerm{
+      (double)(coefficient.size() == 0 ? 1 : std::atoi(coefficient.c_str())),
+      (double)(power.size() == 0 ? 1 : std::atoi(power.c_str())),
+  };
+}
+
+// Assuming integer coefficients
+
+std::vector<PolyTerm> extractTerms(const std::string &str) {
+  std::vector<PolyTerm> result;
+  std::string baseTermExp = "(-?[0-9]*)x(\\^(-?[0-9]*))?";
+  std::regex termReg("\\+" + baseTermExp);
+
+  std::sregex_token_iterator iterator(str.begin(), str.end(), termReg, {1, 3});
+
+  for (; iterator != std::sregex_token_iterator{};) {
+    std::string coeff = *iterator++;
+    std::string power = *iterator++;
+    result.push_back(toTerm(coeff, power));
+  }
+
+  std::regex firstReg(baseTermExp + "\\+.*");
+  std::smatch firstTerm;
+
+  if (std::regex_match(str, firstTerm, firstReg)) {
+    result.push_back(toTerm(firstTerm[1], firstTerm[3]));
+  } else {
+    throw std::runtime_error("Invalid expression");
+  }
+
+  return result;
+}
+
+void intIntegrate(const std::vector<PolyTerm> &polynomial, double start,
+                  double end, double step, std::promise<double> promise) {
+  double result = 0;
+  for (double pos = start; pos < end; pos += step) {
+    result += computePolynomial(polynomial, pos) * step;
+  }
+  promise.set_value(result);
+  return;
+}
+
+double integrate(const std::vector<PolyTerm> &polynomial, size_t noOfThreads,
+                 double start, double end, double step) {
+  std::vector<std::thread> threads;
+  std::vector<std::future<double>> intervals;
+
+  double bigStep = (end - start) / (double)noOfThreads;
+  double result = 0;
+
+  for (size_t i = 0; i < noOfThreads; ++i) {
+    std::promise<double> intervalPromise;
+
+    intervals.push_back(intervalPromise.get_future());
+
+    threads.emplace_back(std::thread(
+        intIntegrate, polynomial, start + i * bigStep,
+        start + (i + 1) * bigStep, step, std::move(intervalPromise)));
+  }
+  for (auto &interval : intervals) {
+    result += interval.get();
+  }
+  for (auto &thread : threads) {
+    thread.join();
+  }
+  return result;
+}
+
+int main(int argc, char **argv) {
+  auto terms = extractTerms("12x+8x");
+  for (const auto &term : terms) {
+    std::cout << term.coeff << "y ^ " << term.power << " + ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "Integral: "
+            << integrate(terms, std::thread::hardware_concurrency(), -3, 3,
+                         0.000001);
+  return 0;
+}
---
+++ Zadanka/MonteCarlo/main.cpp
@@ -0,0 +1,52 @@
+#include <chrono>
+#include <future>
+#include <iostream>
+#include <random>
+
+double monteCarloPi(size_t seed, size_t iterationCount) {
+  std::default_random_engine engine(std::chrono::system_clock().now().time_since_epoch().count() + seed);
+  size_t successCount = 0;
+  std::uniform_real_distribution<double> dist(0, 1);
+  double x, y;
+
+  for (size_t i = 0; i < iterationCount; ++i) {
+    x = dist(engine);
+    y = dist(engine);
+    if (x * x + y * y <= 1) {
+      ++successCount;
+    }
+  }
+  return ((double)(successCount << 2) / (double)iterationCount);
+}
+
+double parallelMonteCarlo(size_t threadCount, size_t iterationCount) {
+  std::vector<std::future<double>> partialResults;
+  std::vector<std::thread> threads;
+
+  for (size_t i = 0; i < threadCount; ++i) {
+    std::packaged_task<double(size_t,size_t)> task(monteCarloPi);
+    partialResults.push_back(task.get_future());
+    threads.emplace_back(std::thread(std::move(task),i, iterationCount));
+  }
+  double result = 0;
+  for (auto &res : partialResults) {
+    auto partial = res.get();
+    std::cout<<"Partial result: "<<partial<<std::endl;
+    result += partial;
+  }
+  for (auto &thread : threads) {
+    thread.join();
+  }
+  return result / (double)threadCount;
+}
+
+// Compute pi in 10 million iterations of monte carlo - then compare sequential
+// and parallel execution times You may use packaged_task
+
+int main(int argc, char **argv) {
+  std::cout.precision(24);
+  std::cout << monteCarloPi(0,100) << std::endl;
+  std::cout << parallelMonteCarlo(std::thread::hardware_concurrency(), 100)
+            << std::endl;
+  return 0;
+}
